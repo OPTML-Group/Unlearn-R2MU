c40ea70d98932a2b8a9524e08bf4d5858406efdc
Changsheng Wang <wangc168@msu.edu>
2025-11-02 16:02:12 -0500

    Update collect_data.py

--- evaluate/collect_data.py
+++ evaluate/collect_data.py
@@ -1,9 +1,8 @@
 import json
 
-# 文件路径
-file_path = "/egr/research-optml/wangc168/reasoning/reason_unlearn/QA_generation/log/merged_with_index_and_non_reason_logits.jsonl"
 
-# 初始化计数器
+file_path = "log/merged_with_index_and_non_reason_logits.jsonl"
+
 count_dict = {
     1: {"non_reason_false": 0, "logits_false": 0},
     2: {"non_reason_false": 0, "logits_false": 0},
@@ -11,7 +10,6 @@ count_dict = {
     4: {"non_reason_false": 0, "logits_false": 0}
 }
 
-# 读取并统计
 with open(file_path, 'r') as f:
     for line in f:
         item = json.loads(line)
@@ -22,6 +20,5 @@ with open(file_path, 'r') as f:
             if not item.get("logits_match", True):
                 count_dict[gold]["logits_false"] += 1
 
-# 输出结果
 for k in range(1, 5):
     print(f"generation_supports_gold == {k}: non_reason_false = {count_dict[k]['non_reason_false']}, logits_false = {count_dict[k]['logits_false']}")
